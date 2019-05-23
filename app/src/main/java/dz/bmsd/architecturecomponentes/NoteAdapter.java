c539a3fddd712967b303da95397d360170b9b041
Billel Boumessaidia <billel.bmsd@gmail.com>
2019-05-23 23:37:39 +0100

    add recycler view

---
+++ app/src/main/java/dz/bmsd/architecturecomponentes/NoteAdapter.java
@@ -0,0 +1,60 @@
+package dz.bmsd.architecturecomponentes;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dz.bmsd.architecturecomponentes.room.Note;
+
+/**
+ * Created by Bilo on 23,May,2019
+ */
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+    private List<Note> notes = new ArrayList<>();
+
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.note_item, viewGroup, false);
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder noteHolder, int i) {
+        Note currentNote = notes.get(i);
+        noteHolder.textPriority.setText(String.valueOf(currentNote.getPriority()));
+        noteHolder.textTitle.setText(currentNote.getTitle());
+        noteHolder.textDescription.setText(currentNote.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.notes.size();
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+        private TextView textTitle;
+        private TextView textDescription;
+        private TextView textPriority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            this.textTitle = itemView.findViewById(R.id.text_title);
+            this.textDescription = itemView.findViewById(R.id.text_description);
+            this.textPriority = itemView.findViewById(R.id.text_priority);
+        }
+    }
+}
